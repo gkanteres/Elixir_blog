960b1a64709529f89e29bd2814f80996c7a40e24
Greg Kanteres <gregkanteres@gmail.com>
2016-04-07 16:14:51 -0700

    Update dev.exs

--- config/dev.exs
+++ config/dev.exs
@@ -35,8 +35,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :pxblog, Pxblog.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "gregory",
-  password: "Propylpower",
+  username: "postgres",
+  password: "postgres",
   database: "pxblog_dev",
   hostname: "localhost",
   pool_size: 10
